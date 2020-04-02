de62f205d266023f79cc942219414d24b282be6f
tavleen68 <57595809+tavleen68@users.noreply.github.com>
2020-04-02 15:10:05 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,9 @@
-INSERT INTO login_details (user_id, password, Name, Account_number) VALUES ("W111","W111@1","Ram",111222331),("W12","W112@2","Raj",111222332),("W113","W113@3","Jas",111222333);
+INSERT INTO login_details (user_id, password, Name, Account_number) VALUES
+("W111","W111@1","Ram",111222331),
+("W12","W112@2","Raj",111222332),
+("W113","W113@3","Jas",111222333);
 
-INSERT INTO Enquiry_details (Account_number, Branch_code, Account_type, Balance, Currency, Status) VALUES (111222331,001,"Savings",222331,"INR","Active"),(111222332,001,"Current",112332,"INR","Active"),(111222333,001,"Savings",11,"INR","Closed");
+INSERT INTO Enquiry_details (Account_number, Branch_code, Account_type, Balance, Currency, Status) VALUES
+(111222331,001,"Savings",222331,"INR","Active"),
+(111222332,001,"Current",112332,"INR","Active"),
+(111222333,001,"Savings",11,"INR","Closed");
